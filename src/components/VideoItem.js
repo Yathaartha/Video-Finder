ed12e3204405310d90b31bd451d2f450d474974b
Yathaartha <maharjanyathaartha017@gmail.com>
2021-05-11 10:54:23 +0545

    video clickable

---
+++ src/components/VideoItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <div
+      onClick={() => onVideoSelect(video)}
+      className="video-item item"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        cursor: "pointer",
+      }}
+    >
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt="thumbnail"
+        style={{ maxWidth: "180px" }}
+      ></img>
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoItem;
